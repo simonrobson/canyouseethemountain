d186cf21014930352dfb13bfe6663fee24928621
Simon Robson <shrobson@gmail.com>
2014-02-22 19:10:40 +0700

    configure page and asset serving

--- server/api/index.js
+++ server/api/index.js
@@ -25,8 +25,9 @@ function configureServer(db) {
   var app = express();
 
   app.use(express.bodyParser());
+  app.use(express.static(__dirname + '/../../public'));
 
-  app.post('/checkins', function(req, res){
+  app.post('/checkins', function(req, res) {
     var checkin = req.param('checkin');
     if (checkin && checkinIsValid(checkin)) {
       db.storeCheckin(checkin);
