bb38d567a2b9274e20f428e4e232d5deac40e162
Rob Burns <rburns@paiges.net>
2014-03-01 20:43:18 +0700

    save location using spatial data type

--- schema.sql
+++ schema.sql
@@ -1,6 +1,7 @@
 CREATE TABLE `landmark` (
 	`id` int(10) unsigned not null auto_increment,
 	`name` varchar(150),
+	`location` point,
 	PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
 
@@ -8,14 +9,14 @@ CREATE TABLE `checkin` (
 	`timestamp` int(11) unsigned,
 	`timezone` int(2),
 	`landmark_id` int(10) unsigned,
-	`lat` float,
-	`lng` float,
+	`location` point,
 	`accuracy` int(10),
 	`visibility` int(10) unsigned,
-	PRIMARY KEY `id` (`timestamp`,`landmark_id`,`lat`,`lng`),
+	PRIMARY KEY `id` (`timestamp`,`landmark_id`,`location`),
 	FOREIGN KEY (landmark_id) REFERENCES landmark(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO landmark (name) VALUES ("Doi Suthep");
+INSERT INTO landmark (name, location)
+	VALUES ("Doi Suthep", GeomFromText("POINT(18.804139 98.922234)"));
 
 
