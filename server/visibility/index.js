317ce4a0783fc171b8f70feb06169c0a5e27a7c7
Rob Burns <rburns@paiges.net>
2014-03-15 20:27:39 +0700

    generate geojson visibility layer, likely still somewhat erroneously

--- server/visibility/index.js
+++ server/visibility/index.js
@@ -1,14 +1,46 @@
 var wktparse = require('wellknown'),
 	db = require('../db');
 
+var grid_size = 4;
+
 var layers = {};
+
+var layerTmpl = {
+	type: 'FeatureCollection',
+	features: []
+}
 
 function visibilityLayer(timestamp, landmark) {
 	return layers[landmark] || {};
 }
 
-function initVisibilityLayer(timestamp, landmark) {
-	db.getCheckinsForDay(timestamp, landmark, updateVibilityLayerWithCheckins(timestamp, landmark));
+function initVisibilityLayer(timestamp, landmark, next) {
+	var processCheckins;
+
+	layers[landmark] = layerTmpl;
+	processCheckins = updateVisibilityLayerWithCheckins(timestamp, landmark, next);
+	db.getCheckinsForDay(timestamp, landmark, processCheckins);
+}
+
+function updateVisibilityLayerWithCheckins(timestamp, landmark, next) {
+	return function(err, checkins) {
+		var i = 0;
+		if( err ) { next(err,null); }
+
+		checkins.forEach(function(checkin) {
+			checkin.coords = {
+				latitude: wktparse(checkin.location).coordinates[0],
+				longitude: wktparse(checkin.location).coordinates[1]
+			};
+			updateVisibilityLayer(timestamp, landmark, checkin, done);
+		});
+
+		function done() {
+			if( ++i === checkins.length ) {
+				next(null, layers[landmark]);
+			}
+		}
+	};
 }
 
 function updateVisibilityLayer(timestamp, landmark, checkin, next) {
@@ -20,21 +52,22 @@ function updateVisibilityLayer(timestamp, landmark, checkin, next) {
 			return;
 		}
 
-		cell = cellForCheckin(checkin, area);
+		if( !layers[landmark] ) {
+		}
 
-/*		getCheckinsInCellForDay(timestamp, landmark, cell, function(err, checkins) {
-			updateLayer(layers[landmark], checkins.reduce(weightedAverage));
+		cell = cellForCheckin(checkin, grid_size);
+
+		db.getCheckinsForDayInCell(timestamp, landmark, cell, function(err, checkins) {
+			if( err ) {
+				next(err, null);
+				return;
+			}
+			if( checkins.length ) {
+				updateGeoJSON(layers[landmark], cell, aggregateVisibility(checkins));
+			}
 			next(null, layers[landmark]);
-    });*/
-	});
-}
-
-function updateVisibilityLayerWithCheckins(timestamp, landmark) {
-	return function(err, checkins) {
-		checkins.each(function(checkin) {
-			updateVisibilityLayer(timestamp, landmark, checkin);
 		});
-	};
+	});
 }
 
 function cellForCheckin(checkin, precision) {
@@ -50,8 +83,48 @@ function cellForCheckin(checkin, precision) {
 	};
 }
 
-function areaFromWKT(area) {
-  return wktparse(area).coordinates[0].slice(0,4);
+function aggregateVisibility(checkins) {
+	var weights = [10, 9, 8 , 7, 6, 5, 4, 3, 2 , 1, 0];
+	return checkins.reduce(weightedSum(weights), 0) / checkins.length / 10
+}
+
+function updateGeoJSON(layer, cell, visibility) {
+	var index, id;
+
+	id =  cellId(cell);
+	existing = objById(layer.features, id);
+
+	if( existing == null ) {
+		layer.features.push({
+			type: 'Feature',
+			id: id,
+			properties: {visibility: visibility},
+			geometry: cell
+		});
+	} else {
+		existing.properties.visibility = visibility;
+	}
+}
+
+function cellId(cell) {
+	return cell.coordinates[0].map(function(coord) { return coord.join('|'); }).join('||');
+}
+
+function objById(collection, id) {
+	var result = null;
+	collection.forEach(function(obj) {
+		if( result === null && obj.id === id ) {
+			result = obj;
+		}
+	});
+	return result;
+}
+
+function weightedSum(weights) {
+	return function(memo, checkin) {
+		memo += weights[checkin.age] || 0 * checkin.visibility;
+		return memo;
+	};
 }
 
 function roundDown(number, precision) {
