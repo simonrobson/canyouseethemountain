135d3049f124f1614bd01c4c35c8e7535e70ffff
Rob Burns <rburns@paiges.net>
2016-02-11 22:01:58 +0700

    don't swap lat/lng for the benfit of mysql

--- server/db/index.js
+++ server/db/index.js
@@ -51,8 +51,8 @@ function storeCheckin(checkin, next) {
   values = [
     checkin.timezone,
     checkin.landmark_id,
-    checkin.coords.latitude,
     checkin.coords.longitude,
+    checkin.coords.latitude,
     checkin.coords.accuracy || 'NULL',
     checkin.visibility
   ];
@@ -89,9 +89,10 @@ function getCheckinsForDay(timestamp, landmark, next) {
 function nearLandmark(coords, id, next) {
   var processResult, values;
 
+  console.log
   field = "ST_Contains(area::geometry, ST_SetSRID(ST_Point($1, $2), 4326)::geometry) AS near";
 
-  values = [parseFloat(coords.latitude), parseFloat(coords.longitude), id];
+  values = [parseFloat(coords.longitude), parseFloat(coords.latitude), id];
 
   next = next || function() {};
 
--- test/cell_for_checkin.js
+++ test/cell_for_checkin.js
@@ -21,19 +21,19 @@ describe('cellForCheckin', function() {
 
 	it("produces a cell with dimension .001 for precision of three", function() {
 		var result = visibility.cellForCheckin(checkin, 3);
-		assert.deepEqual(result.coordinates[0][0],[18.807, 98.946]);
-		assert.deepEqual(result.coordinates[0][1],[18.807, 98.947]);
-		assert.deepEqual(result.coordinates[0][2],[18.806, 98.947]);
-		assert.deepEqual(result.coordinates[0][3],[18.806, 98.946]);
-		assert.deepEqual(result.coordinates[0][4],[18.807, 98.946]);
+		assert.deepEqual(result.coordinates[0][0],[98.946, 18.807]);
+		assert.deepEqual(result.coordinates[0][1],[98.947, 18.807]);
+		assert.deepEqual(result.coordinates[0][2],[98.947, 18.806]);
+		assert.deepEqual(result.coordinates[0][3],[98.946, 18.806]);
+		assert.deepEqual(result.coordinates[0][4],[98.946, 18.807]);
 	});
 
 	it("produces a cell with dimension .0001 for precision of four", function() {
 		var result = visibility.cellForCheckin(checkin, 4);
-		assert.deepEqual(result.coordinates[0][0],[18.8065, 98.9466]);
-		assert.deepEqual(result.coordinates[0][1],[18.8065, 98.9467]);
-		assert.deepEqual(result.coordinates[0][2],[18.8064, 98.9467]);
-		assert.deepEqual(result.coordinates[0][3],[18.8064, 98.9466]);
-		assert.deepEqual(result.coordinates[0][4],[18.8065, 98.9466]);
+		assert.deepEqual(result.coordinates[0][0],[98.9466, 18.8065]);
+		assert.deepEqual(result.coordinates[0][1],[98.9467, 18.8065]);
+		assert.deepEqual(result.coordinates[0][2],[98.9467, 18.8064]);
+		assert.deepEqual(result.coordinates[0][3],[98.9466, 18.8064]);
+		assert.deepEqual(result.coordinates[0][4],[98.9466, 18.8065]);
 	});
 });
